558ec08207e37a6b997ef30536f1a611477a1a52
Siddhant Mishra <90592310+0NE-C0DEMAN@users.noreply.github.com>
2022-04-28 17:46:46 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Distutils',
+    version='1.0',
+    description='Python Distribution Utilities',
+    author='Prakhar',
+    author_email='prakhar.parihar@celebaltech.com',
+    url='https://www.python.org/sigs/distutils-sig/',
+    packages=['distutils', 'distutils.command'],
+)
